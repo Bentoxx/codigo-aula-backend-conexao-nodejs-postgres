f7e59398974ff4075e4255c6f3a8b1c597f05749
Bento Netto <bentoeemanuele@gmail.com>
2024-08-17 12:39:14 -0300

    Implantado o tratamento de erros por conta da assincronicidade da aplicação com o bloco trycatch

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,25 @@ const app = express()
 app.use(express.json())
 
 app.get('/', async(req, res) => {
-	const resposta = await conexao.query('select * from empresas')
-
-
-	return res.json(resposta.rows)
+	try {
+		const resposta = await conexao.query('select * from empresas')
+		return res.json(resposta.rows)
+		
+	} catch (error) {
+		const erro = error as Error
+		return res.status(400).json(erro.message) 
+	}
+})
+app.get('/:id', async(req, res) => {
+	const { id } = req.params
+	try {
+		const resposta = await conexao.query('select * from empresas where id = $1 and nome = $2', [id, 'Yahoo'])
+		return res.json(resposta.rows)
+		
+	} catch (error) {
+		const erro = error as Error
+		return res.status(400).json(erro.message)
+	}
 })
 
 
