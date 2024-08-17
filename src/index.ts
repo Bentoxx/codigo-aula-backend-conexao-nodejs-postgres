d8f8433516fcfc5125ffac5626a1d1593bf101fa
Bento Netto <bentoeemanuele@gmail.com>
2024-08-17 11:51:36 -0300

    Criação da conexão client com o banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import 'dotenv/config'
 
 import express from 'express'
+import { client } from './conexao'
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-	return res.send('Ok')
+app.get('/', async(req, res) => {
+	await client.connect()
+	const resposta = await client.query('select * from empresas')
+	await client.end()
+
+	return res.json(resposta)
 })
 
 
